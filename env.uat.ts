eadc49a1070387150c317542fde017c050bdc771
OThomps <othompson@qualityworkscg.com>
2024-03-26 12:43:42 -0500

    updated uat cookies

--- env.uat.ts
+++ env.uat.ts
@@ -4,13 +4,13 @@ export const environment = {
     //unable to login
     cookies: {
         name: 'SSESSb3942a91b5710bb8581186c1f2753872',
-        value: 'fc6MqrtsoDtXfMrczSx0qAkDyYJVoSmQhCnCpXjXwJ88Phjw',
+        value: '9md0XUM7uTQjCxks4Nu5YwhZ4qWAqBrH7M36Ktb2xz6tAcaG',
         domain: '.uat-content.montefioreeinstein.org',
         path: '/',
     },
     admin: {
         name: 'SSESSb3942a91b5710bb8581186c1f2753872',
-        value: 'fc6MqrtsoDtXfMrczSx0qAkDyYJVoSmQhCnCpXjXwJ88Phjw',
+        value: 'TUHiIg1cVV-AjLxwf6IZ0lbwtq7vy%2CTRoVCO4605CzFk%2CO9O',
         domain: '.uat-content.montefioreeinstein.org',
         path: '/',
     }
